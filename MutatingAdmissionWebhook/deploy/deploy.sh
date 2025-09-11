15b2622569eae1aca56bd015493cb356f45e2055
Ashish Kumar <ashishkumar@Ashishs-MacBook-Pro.local>
2025-09-12 01:34:11 +0530

    update

--- MutatingAdmissionWebhook/deploy/deploy.sh
+++ MutatingAdmissionWebhook/deploy/deploy.sh
@@ -18,24 +18,22 @@ ST = State
 L = Locality
 O = Organization
 OU = Unit
-CN = mutate-monitoring-label.webhook.svc
+CN = mutate-sidecar.webhook.svc
 [ req_ext ]
 subjectAltName = @alt_names
 [ alt_names ]
-DNS.1 = mutate-monitoring-label.webhook.svc
-DNS.2 = mutate-monitoring-label.webhook.svc.cluster.local
+DNS.1 = mutate-sidecar.webhook.svc
+DNS.2 = mutate-sidecar.webhook.svc.cluster.local
 EOF
 
 openssl req -new -sha256 -nodes -out $CERT_DIR/webhook.csr -newkey rsa:2048 -keyout $CERT_DIR/webhook.key -config $CERT_DIR/webhook-csr.conf
 openssl x509 -req -in $CERT_DIR/webhook.csr -signkey $CERT_DIR/webhook.key -out $CERT_DIR/webhook.crt -days 365 -extensions req_ext -extfile $CERT_DIR/webhook-csr.conf
 
-# Install Helm chart
-cd chart
 
 # check diff
-helm -n webhook diff upgrade --install mutating-webhook --set app.image=<image:tag> .
+# helm -n webhook diff upgrade --install mutating-webhook --set app.image=ashishkumar256/mutating-webhook:1 chart
 
 # deploy
-helm -n webhook upgrade --install mutating-webhook --set app.image=<image:tag> .
+helm -n webhook upgrade --install mutating-webhook --set app.image=ashishkumar256/mutating-webhook:1 chart
 
-Note: Image was created while doing activity, you may use it - "ashishkumar256/mutate-webhook:latest"
+# Note: Image was created while doing activity, you may use it - "ashishkumar256/mutate-webhook:1"
